3c34cab0bf6dfd3327c3e53e85e76e6d2b30a376
Timbaro9 <tmtmosweu@gmail.com>
2025-04-25 12:21:47 +0200

    Add Stripe configuration and credentials for development environment

---
+++ config/initializers/stripe.rb
@@ -0,0 +1,6 @@
+Rails.configuration.stripe = {
+  publishable_key: Rails.application.credentials.dig(:stripe, :pk),
+  secret_key: Rails.application.credentials.dig(:stripe, :sk)
+}
+
+Stripe.api_key = Rails.configuration.stripe[:secret_key]
